f2a7c860ca926075577475cecd37d4a0f36d305e
jhonatasantunes1605 <157020366+jhonatasantunes1605@users.noreply.github.com>
2024-04-03 13:31:11 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,10 +1,26 @@
 <?php
-// URL do site que você deseja acessar
+// Obtenha a URL da consulta do parâmetro GET
 $url = $_GET['url'];
 
-// Faz a solicitação para a URL desejada
-$response = file_get_contents($url);
+// Verifique se a URL é válida
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    // Permitir apenas URLs HTTP e HTTPS
+    if (strpos($url, 'http://') !== 0 && strpos($url, 'https://') !== 0) {
+        $url = 'http://' . $url;
+    }
 
-// Retorna a resposta para o cliente
-echo $response;
+    // Busque o conteúdo da URL usando cURL
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Imprima o conteúdo da resposta
+    echo $response;
+} else {
+    // Se a URL não for válida, retorne um erro
+    http_response_code(400);
+    echo "URL inválida";
+}
 ?>
